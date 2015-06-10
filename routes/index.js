c55b751f65480843ddb9adb35596c1b63c327467
unknown <dimitar.dzhondzhorov@DimitarDzh.Axs-offices.com>
2015-06-10 17:25:15 +0300

    Getting there.

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,20 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+app.get('/test', function(req, res) {
+  var info = "some test request";
+  console.log("----------------------------------------------------------------");
+  io.sockets.emit("news", {info: info});
+  res.send('');
+});
+
+
 io.on('connection', function (socket) {
 	console.log(socket);
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
-
+    io.sockets.emit('message', data);
   // console.log('a user connected');
   });
 });
