549b863655e3efdf7fd8460ac5b552fffef407f4
unknown <dimitar.dzhondzhorov@DimitarDzh.Axs-offices.com>
2015-06-10 16:41:21 +0300

    Creating simple socket.io app.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var app = require('express')();
+// var server = require('http').createServer(app);
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
 var router = express.Router();
 
+// server.listen(8088);
+// server.listen(app.get('port'), function(){
+//   console.log('listening on *:' + app.get('port'));
+// });
+
 /* GET home page. */
-router.get('/', function(req, res) {
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Express' });
+// });
+app.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+io.on('connection', function (socket) {
+	console.log(socket);
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+
+  // console.log('a user connected');
+  });
+});
+
+module.exports = app;
